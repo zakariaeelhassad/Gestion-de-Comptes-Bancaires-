20b5e9e3b65831778d7f62c28d20b180259314d3
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2025-09-18 14:18:45 +0100

    ajouter la class CompteService dans le dossier presentation

---
+++ src/services/CompteService.java
@@ -0,0 +1,41 @@
+package services;
+
+import métier.Compte;
+import utilitaires.ValidationUtils;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class CompteService {
+
+	private Map<String , Compte> comptes = new HashMap<>();
+	
+	public String genererCodeCompte() {
+        Random rand = new Random();
+        String code;
+
+        do {
+            int nombre = rand.nextInt(90000) + 10000; // بين 10000 و 99999
+            code = "CPT-" + nombre;
+        } while (comptes.containsKey(code)); // نعاود توليد حتى يكون unique
+
+        return code;
+    }
+
+    public void ajouterCompte(Compte compte) {
+        if (ValidationUtils.validerCode(compte.getCode())) {
+            comptes.put(compte.getCode(), compte);
+            System.out.println("Compte ajouté avec succès: " + compte.getCode());
+        } else {
+            System.out.println("Code invalide !");
+        }
+    }
+	
+	public Compte chercherCompte(String code) {
+		return comptes.get(code);
+	}
+	
+	public Map<String , Compte> getComptes() {
+		return comptes ;
+	}
+}
