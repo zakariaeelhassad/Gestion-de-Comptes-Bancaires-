3131c585ea77bd53d6371a3d5a3859fccd4e0bb2
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2025-09-19 09:41:10 +0100

    modifier dans la class validationUtils

--- src/utilitaires/ValidationUtils.java
+++ src/utilitaires/ValidationUtils.java
@@ -3,7 +3,7 @@ package utilitaires;
 public class ValidationUtils {
 
 	public static boolean validerCode(String code) {
-        // خاصو يبدا بـ CPT- و من بعد 5 أرقام
+   
         return code.matches("CPT-\\d{5}");
     }
 	
